54665bfe0ad3873e8a0b8208e6e2c27758e849cb
ArhabSafwan <asafwan72@gmail.com>
2025-07-13 21:30:23 +0600

    props

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,29 @@
-function ListGroup() {
+import { useState } from "react";
+
+interface ListGroupProps {
+  items: string[];
+  heading?: string;
+}
+function ListGroup({ items, heading }: ListGroupProps) {
+  const [selectedIndex, setSelectedIndex] = useState(-1); // Initialize selectedIndex to -1
+
   return (
     <>
-        <h1>List</h1>
-        <ul className="list-group">
-          <li className="list-group-item">An Item</li>
-          <li className="list-group-item">An Item</li>
-          <li className="list-group-item">An Item</li>
-          <li className="list-group-item">An Item</li>
-          <li className="list-group-item">An Item</li>
-        </ul>
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>No items found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={`list-group-item ${
+              selectedIndex === index ? "active" : ""
+            }`}
+            key={item}
+            onClick={() => setSelectedIndex(index)}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
