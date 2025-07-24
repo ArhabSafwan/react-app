4249aeae9dbd95cb9c80e924d3d8ef7bc4e42ffa
ArhabSafwan <asafwan72@gmail.com>
2025-07-24 09:13:24 +0600

    rendering list items

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,27 @@ import { useState } from "react";
 import Student from "./components/Student";
 import UserGreeting from "./components/UserGreeting";
 import List from "./components/List";
-//props: read only properties that can be shared from parent component 
+//props: read only properties that can be shared from parent component
 // to child component
 function App() {
-  const [alertVisible, setAlertVisible] = useState(false);
+  const fruits = [
+    { id: 1, name: "orange", calories: 233 },
+    { id: 2, name: "banana", calories: 45 },
+    { id: 3, name: "apple", calories: 65 },
+    { id: 4, name: "grapes", calories: 543 },
+    { id: 5, name: "berries", calories: 87 },
+  ];
+  const vegetables = [
+    { id: 1, name: "carrot", calories: 41 },
+    { id: 2, name: "broccoli", calories: 55 },
+    { id: 3, name: "spinach", calories: 23 },
+    { id: 4, name: "peas", calories: 81 },
+    { id: 5, name: "kale", calories: 49 },
+  ];
   return (
     <div>
-      {/* <UserGreeting isUserLogged={false} name="Arhab"/> */}
-      <List/>
+      <List items={fruits} category="fruits" />
+      <List items={vegetables} category="vegetables" />
     </div>
   );
 }
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,29 +1,32 @@
 import React from "react";
+const List = (props) => {
+  const itemList = props.items;
+  const category = props.category;
 
-const List = () => {
-  const fruits = [
-    { id: 1, name: "orange", calories: 233 },
-    { id: 2, name: "banana", calories: 45 },
-    { id: 3, name: "apple", calories: 65 },
-    { id: 4, name: "grapes", calories: 543 },
-    { id: 5, name: "berries", calories: 87 },
-  ];
-  //   fruits.sort((a,b) => a.name.localeCompare(b.name)); //alphabetic
-//   fruits.sort((a, b) => b.name.localeCompare(a.name)); // reverse alphabetic
-
-// const highcCalFruits = fruits.filter(fruit=> fruit.calories > 100); finding high cal fruit
-// const lowcCalFruits = fruits.filter(fruit => fruit.calories<100);  low cal fruit
-
-  const listItems = fruits.map((fruit) => (
-    <li key={fruit.id}>
-      {fruit.name} <b>{fruit.calories}</b>
-    </li>
-  ));
+  const listItems = itemList.map(
+    (item) => (
+      <li key={item.id}>
+        {item.name} <b>{item.calories}</b>
+      </li>
+    )
+  );
   return (
     <div>
+      <h2>{category}</h2>
       <ol>{listItems}</ol>
     </div>
   );
 };
 
 export default List;
+
+
+
+
+
+
+//   fruits.sort((a,b) => a.name.localeCompare(b.name)); //alphabetic
+//   fruits.sort((a, b) => b.name.localeCompare(a.name)); // reverse alphabetic
+
+// const highcCalFruits = fruits.filter(fruit=> fruit.calories > 100); finding high cal fruit
+// const lowcCalFruits = fruits.filter(fruit => fruit.calories<100);  low cal fruit
